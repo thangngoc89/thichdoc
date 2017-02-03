b94118f1d04da2b719c195974075ebd5ae166eea
Khoa Nguyen <khoa02031995@gmail.com>
2017-02-03 22:36:29 +0700

    Fix props on meta Tag

--- components/App.js
+++ components/App.js
@@ -6,7 +6,7 @@ import Head from "next/head";
 export default ({ children, title }) => (
   <div>
     <Head>
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="stylesheet" href="/static/css/tachyons.min.ai.css" />
       <link
