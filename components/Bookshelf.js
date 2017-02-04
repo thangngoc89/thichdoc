92c7ee61f5dfbd2562659c86fa3d75655265aea5
Khoa Nguyen <khoa02031995@gmail.com>
2017-02-04 12:22:31 +0700

    Dont put white bg to top of the book img

--- components/Bookshelf.js
+++ components/Bookshelf.js
@@ -2,14 +2,14 @@ import React from "react";
 import GridFluidFourMax from "./BuildBlocks/GridFluidFourMax";
 
 const Book = ({ i }) => (
-  <article className="bg-white mh3 flex flex-column justify-end h-100">
+  <article className="mh3 flex flex-column justify-end h-100">
     <a className="link">
       <img
         src={`/static/books/book${+i + 1}.jpg`}
         className="w-100 bg-light-silver dim bb bw3 b--red ma0 pa0"
       />
     </a>
-    <div className="pa3">
+    <div className="pa3 bg-white ">
       <h1 className="f3 mb2">Mimi W.</h1>
       <h2 className="f5 fw4 gray mt0">CCO (Chief Cat Officer)</h2>
     </div>
