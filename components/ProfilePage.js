c939f938e2de1487d6e37b08aa84a338db8e1f49
Khoa Nguyen <khoa02031995@gmail.com>
2017-02-05 23:52:45 +0700

    Make UserInfoCard text center

--- components/ProfilePage.js
+++ components/ProfilePage.js
@@ -16,7 +16,7 @@ const Cover = () => (
   </div>
 );
 const UserInfo = ({ avatar, job, name, recommendationsCount }) => (
-  <Card className="flex flex-column items-center">
+  <Card className="flex flex-column items-center tc">
     <figure>
       <img
         className="br-100 pa1 mb2 bg-white h4 w4 nt5 shadow-5"
