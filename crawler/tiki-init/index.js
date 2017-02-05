6bc7125ba1ae9effca30ebc6a0e93812e5344715
Khoa Nguyen <khoa02031995@gmail.com>
2017-02-05 20:17:32 +0700

    Finished with fetching initial daa

--- crawler/tiki-init/index.js
+++ crawler/tiki-init/index.js
@@ -3,6 +3,7 @@
  */
 const got = require("got");
 const cheerio = require("cheerio");
+const fs = require("mz/fs");
 
 const devUrl = "http://localhost:8080/tiki.html";
 const prodUrl = "https://tiki.vn/nguoi-noi-tieng-doc-gi";
@@ -28,14 +29,21 @@ const recursiveTrim = data => {
   return result;
 };
 
+const cleanUpComment = comment => {
+  if (typeof comment !== "string") {
+    return comment;
+  }
+  return comment.replace(/(\n|\t)/g, "");
+};
+
 async function main() {
   try {
     const { body } = await Promise.resolve(got(prodUrl));
     const $ = cheerio.load(body);
 
-    $(".tacgia__line-wr").each(function(i, elem) {
-      console.log($(this).html());
-      process.exit(1);
+    $(".tacgia__line-wr").each(function() {
+      // console.log($(this).html());
+      // process.exit(1);
       const avatar = $(this).find("img.tacgia__line-photo").data("original");
       if (typeof avatar === "undefined") {
         return;
@@ -43,15 +51,30 @@ async function main() {
       const author = $(this).find("p > strong.ribbon-content").text();
       const bio = $(this).find("p.tacgia__line-desc").text();
       const tikiLink = $(this).find("a.tacgia__line-more").attr("href");
+      const recommendations = [];
+      const books = $(this)
+        .find(".tacgia__line-right > .tacgia__book-item")
+        .each(function() {
+          recommendations.push({
+            img: $(this).find("img.tacgia__book-img").data("original"),
+            link: $(this).find("a.book_item-buy").attr("href"),
+            name: $(this).find(".tacgia__book-name").text(),
+            author: $(this).find(".tacgia__book-author").text(),
+            comment: cleanUpComment(
+              $(this).find(".book__comment > .comment-content").text()
+            )
+          });
+        });
       const user = {
         avatar,
         author,
         bio,
-        tikiLink
+        tikiLink,
+        books: recommendations
       };
       data.push(recursiveTrim(user));
     });
-    console.log(data);
+    await fs.writeFile("./data.json", JSON.stringify(data, null, 2));
   } catch (err) {
     console.error(err);
   }
