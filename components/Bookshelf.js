d0748c878209172c0a184358f387e8963713a30f
Khoa Nguyen <khoa02031995@gmail.com>
2017-02-06 02:01:28 +0700

    Try to fix bookshelf bug on safari

--- components/Bookshelf.js
+++ components/Bookshelf.js
@@ -5,15 +5,25 @@ import GridFluidFourMax from "./BuildBlocks/GridFluidFourMax";
 const Book = ({ author, cover, name }) => (
   <article className="mh4 mh3-ns flex flex-column justify-end h-100">
     <a className="link">
+      <style jsx>
+        {
+          `
+          img {
+            max-height: 25em;
+          }
+          `
+        }
+      </style>
       <img
         src={cover}
         className="w-100 bg-light-silver dim bb bw3 b--red ma0 pa0"
       />
     </a>
-    <div className="pa3 bg-white shadow-4">
+    <div className="flex flex-column justify-center pa3 bg-white h4 shadow-4">
       <h1 className={cl("mb2", {
           f3: name.length < 12,
-          f4: name.length >= 12
+          f4: name.length >= 12,
+          f5: name.length >= 20
         })}>{name}</h1>
       <h2 className="f5 fw4 gray mt0">{author}</h2>
     </div>
