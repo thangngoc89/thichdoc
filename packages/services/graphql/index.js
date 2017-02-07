fcbd5d3f99d071d77ca308764e7de5f2e3e3a579
Khoa Nguyen <khoa02031995@gmail.com>
2017-02-07 21:00:37 +0700

    Clean up task

--- packages/services/graphql/index.js
+++
@@ -1,5 +0,0 @@
-const server = require("graphql-server-lambda");
-const schema = require("./schema");
-
-server.graphqlLambda({ schema, context: {} });
-// module.exports.graphiql = server.graphqilLambda({ endpointURL: "/graphql" });
