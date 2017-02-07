19973cd256135225fa5420df3c571eaf9a7b9a81
Khoa Nguyen <khoa02031995@gmail.com>
2017-02-07 21:06:00 +0700

    Move frontend related code to /src
    Refactor to support the movement

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 const { createServer } = require("http");
 const { parse } = require("url");
+const { resolve } = require("path");
 const next = require("next");
-
-const dev = process.env.NODE_ENV !== "production";
-console.log(process.env.NODE_ENV);
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
-const port = process.env.PORT || 3000;
-
 const router = require("./router");
+
+const PORT = process.env.PORT || 3000;
+
+const app = next({
+  dev: process.env.NODE_ENV !== "production",
+  dir: resolve(__dirname + "/src")
+});
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const { pathname } = parse(req.url, true);
 
     const profileQuery = router.profile.match(pathname);
-    if (router.profile.match(pathname)) {
+    if (profileQuery) {
       app.render(req, res, "/profile", profileQuery);
     } else {
       handle(req, res);
     }
-  }).listen(port, err => {
+  }).listen(PORT, err => {
     if (err) throw err;
-    console.log("> Ready on http://localhost:" + port);
+    console.log("> Ready on http://localhost:" + PORT);
   });
 });
---
+++ src/README.md
@@ -0,0 +1,1 @@
+This folder contains Next.js frontend related code
