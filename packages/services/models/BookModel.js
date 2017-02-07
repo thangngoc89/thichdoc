938d4cda40d874c33f8db5bd0905b0410813dc97
Khoa Nguyen <khoa02031995@gmail.com>
2017-02-08 04:32:18 +0700

    It's time for debuging

--- packages/services/models/BookModel.js
+++ packages/services/models/BookModel.js
@@ -1,28 +1,26 @@
-const data = require("../book.json");
+const data = require("../books.json");
 const UserModel = require("./UserModel");
 
-class BookModel {
-  findByIds = ids => {
+const BookModel = {
+  findByIds: ids => {
     return data.filter(b => ids.indexOf(b.id) > -1);
-  };
-
-  findById = id => {
+  },
+  findById: id => {
     return data.find(b => b.id === id);
-  };
-
-  findAll = (skip = 0, limit = 20) => {
+  },
+  findAll: (skip = 0, limit = 20) => {
     if (limit > 20) {
       throw new Error("Can't query more than 20 records");
     }
     return data.slice(skip, skip + limit);
-  };
-
-  findRecommendByById = id => {
+  },
+  findRecommendByById: id => {
     // NOTE: We should have comment here too just
     // in case I forget about it
-    const userIds = this.findById(id).recommendBy;
+    console.log(UserModel);
+    const userIds = BookModel.findById(id).recommendBy;
     return UserModel.findByIds(userIds);
-  };
-}
+  }
+};
 
 module.exports = BookModel;
--- packages/services/models/UserModel.js
+++ packages/services/models/UserModel.js
@@ -1,32 +1,28 @@
-const data = require("../user.json");
+const data = require("../users.json");
 const BookModel = require("./BookModel");
 
-class UserModel {
-  findByIds = ids => {
+const UserModel = {
+  findByIds: ids => {
     return data.filter(u => ids.indexOf(u.id) > -1);
-  };
-
-  findByUsername = username => {
-    return data.find(u => u.username === username);
-  };
-
-  findById = id => {
+  },
+  findById: id => {
     return data.find(u => u.id === id);
-  };
-
-  findAll = (skip = 0, limit = 20) => {
+  },
+  findByUsername: username => {
+    return data.find(u => u.username === username);
+  },
+  findAll: (skip = 0, limit = 20) => {
     if (limit > 20) {
       throw new Error("Can't query more than 20 records");
     }
     return data.slice(skip, skip + limit);
-  };
-
-  findRecommendBooksById = id => {
-    const bookIds = this.findById(id).books;
+  },
+  findRecommendBooksById: id => {
+    const bookIds = UserModel.findById(id).books;
 
     // NOTE: We'll also have comment here, it was ommitted during mocking
-    return Book.findByIds(bookIds);
-  };
-}
+    return BookModel.findByIds(bookIds);
+  }
+};
 
 module.exports = UserModel;
