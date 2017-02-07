ac418fc00773e4c4e15ece37ebd3c780743f730d
Khoa Nguyen <khoa02031995@gmail.com>
2017-02-07 10:58:57 +0700

    Tell GraphQL to auto resolve circular dependencies

--- packages/services/graphql/resolvers.js
+++ packages/services/graphql/resolvers.js
@@ -32,55 +32,18 @@ const allBooks = _.chain(data)
 
 const resolveFunctions = {
   Query: {
-    Book(_, { id, slug }, undefined, info) {
-      const book = allBooks.find(book => book.slug === id);
-      if (hasField(info, "recommendBy")) {
-        const userRecommendedThisBook = allUsers.filter(user => {
-          if (
-            typeof user.books !== "undefined" &&
-              Boolean(user.books.find(b => b.name === book.name))
-          ) {
-            return true;
-          }
-          return false;
-        });
-        book.recommendBy = userRecommendedThisBook.map(u => ({
-          user: u,
-          content: u.books.find(b => b.name === book.name).comment
-        }));
-      }
-      return book;
+    Book: (_, { id, slug }, undefined, info) => {
+      allBooks.find(book => book.slug === id);
     },
-    allBooks(_, { skip = 0, limit = 20 }, __, info) {
-      const res = [...allBooks.slice(skip, skip + limit)];
-      if (hasField(info, "recommendBy")) {
-        throw new Error(
-          "Query recommendBy on allBooks operation is not allowed"
-        );
-      }
-      return res;
-    },
-    User(_, { username }) {
-      const user = allUsers.find(u => u.username === username);
-      if (typeof user !== "undefined") {
-        user.recommendBooks = user
-          .books.map(alterBookField)
-          .map(b => ({ book: b, content: b.comment }));
-      }
-      return user;
-    },
-    allUsers(_, { skip = 0, limit = 20 }, __, info) {
-      const users = allUsers.slice(skip, skip + limit);
-      if (hasField(info, "recommendBooks")) {
-        throw new Error(
-          "Query recommendBooks on allUsers operation is not allowed"
-        );
-      }
-      return user;
-    }
+    allBooks: (_, { skip = 0, limit = 20 }, __, info) => [
+      ...allBooks.slice(skip, skip + limit)
+    ],
+    User: (_, { username }) => allUsers.find(u => u.username === username),
+    allUsers: (_, { skip = 0, limit = 20 }, __, info) =>
+      allUsers.slice(skip, skip + limit)
   },
   Mutation: {
-    addBook(_, { postId }) {
+    addBook: (_, { postId }) => {
       const post = _.find(posts, { id: postId });
       if (!post) {
         throw new Error(`Couldn't find post with id ${postId}`);
@@ -88,6 +51,30 @@ const resolveFunctions = {
       post.votes += 1;
       return post;
     }
+  },
+  Book: {
+    recommendBy: book => {
+      const userRecommendedThisBook = allUsers.filter(user => {
+        if (
+          typeof user.books !== "undefined" &&
+            Boolean(user.books.find(b => b.name === book.name))
+        ) {
+          return true;
+        }
+        return false;
+      });
+      if (!userRecommendedThisBook) {
+        return null;
+      }
+      return userRecommendedThisBook.map(u => ({
+        user: u,
+        content: u.books.find(b => b.name === book.name).comment
+      }));
+    }
+  },
+  User: {
+    recommendBooks: user =>
+      user.books.map(alterBookField).map(b => ({ book: b, content: b.comment }))
   }
 };
 
