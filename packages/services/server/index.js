0d6aa5510fc5c0c0e2395859b7c2b893f94dceaf
Khoa Nguyen <khoa02031995@gmail.com>
2017-02-11 05:32:40 +0700

    Allow cors for backend

--- packages/services/server/index.js
+++ packages/services/server/index.js
@@ -8,5 +8,14 @@ const PORT = 3000;
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://thichdoc.com");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
 app.listen(PORT);
