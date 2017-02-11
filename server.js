0ccf96f6001458f12dbf5109306ecf3bdb1ba8aa
Khoa Nguyen <khoa02031995@gmail.com>
2017-02-11 15:04:02 +0700

    Handle /static folder with correct cache control
    Enable ssr caching

--- server.js
+++ server.js
@@ -9,15 +9,22 @@ const log = s => {
 const PORT = process.env.PORT || 3000;
 const app = next({
   dev: process.env.NODE_ENV !== "production",
-  dir: resolve(__dirname + "/src")
+  dir: resolve(__dirname, "src")
 });
 const handle = app.getRequestHandler();
+
+const ssrCache = new LRUCache({
+  max: 100,
+  maxAge: 1000 * 60 * 60 // 1hour
+})
 
 app.prepare().then(() => {
   const server = express();
 
+  server.use('/static', express.static(resolve(__dirname, "src", "static"), , { maxAge: '365d' }))
+
   server.get("/u/:username", (req, res) => {
-    return app.render(req, res, "/profile", req.params);
+    return renderAndCache(req, res, "/profile", req.params);
   });
 
   server.get("*", (req, res) => handle(req, res));
@@ -27,3 +34,25 @@ app.prepare().then(() => {
     console.log("> Ready on http://localhost:" + PORT);
   });
 });
+
+function renderAndCache (req, res, pagePath, queryParams) {
+  // If we have a page in the cache, let's serve it
+  if (ssrCache.has(req.url)) {
+    console.log(`CACHE HIT: ${req.url}`)
+    res.send(ssrCache.get(req.url))
+    return
+  }
+
+  // If not let's render the page into HTML
+  app.renderToHTML(req, res, pagePath, queryParams)
+    .then((html) => {
+      // Let's cache this page
+      log(`CACHE MISS: ${req.url}`)
+      ssrCache.set(req.url, html)
+
+      res.send(html)
+    })
+    .catch((err) => {
+      app.renderError(err, req, res, pagePath, queryParams)
+    })
+}
